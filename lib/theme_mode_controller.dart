e30cd9e0e40b7429be64c660b4a50ee49864510b
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-03-06 03:00:33 +0100

    Refactor theme mode management to use ValueNotifier and InheritedNotifier

--- lib/theme_mode_controller.dart
+++ lib/theme_mode_controller.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
 
-// Using ValueNotifier instead of ChangeNotifier
-class ThemeModeController {
-  final ValueNotifier<ThemeMode> _themeMode = ValueNotifier<ThemeMode>(
-    ThemeMode.system,
-  );
-
-  ValueNotifier<ThemeMode> get themeMode => _themeMode;
+class ThemeModeNotifier extends ValueNotifier<ThemeMode> {
+  ThemeModeNotifier() : super(ThemeMode.system);
 
   void updateThemeMode(ThemeMode themeMode) {
-    _themeMode.value = themeMode;
-  }
-
-  void dispose() {
-    _themeMode.dispose();
+    value = themeMode;
   }
 }
 
-// Using ChangeNotifier instead of ValueNotifier
-class ThemeModeNotifier extends ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.system;
+// class ThemeModeController {
+//   final ValueNotifier<ThemeMode> _themeMode = ValueNotifier<ThemeMode>(
+//     ThemeMode.system,
+//   );
 
-  ThemeMode get themeMode => _themeMode;
+//   ValueNotifier<ThemeMode> get themeMode => _themeMode;
 
-  void updateThemeMode(ThemeMode themeMode) {
-    _themeMode = themeMode;
-    notifyListeners();
-  }
-}
+//   void updateThemeMode(ThemeMode themeMode) {
+//     _themeMode.value = themeMode;
+//   }
+
+//   void dispose() {
+//     _themeMode.dispose();
+//   }
+// }
+
+// // Using ChangeNotifier instead of ValueNotifier
+// class ThemeModeNotifier extends ChangeNotifier {
+//   ThemeMode _themeMode = ThemeMode.system;
+
+//   ThemeMode get themeMode => _themeMode;
+
+//   void updateThemeMode(ThemeMode themeMode) {
+//     _themeMode = themeMode;
+//     notifyListeners();
+//   }
+// }
