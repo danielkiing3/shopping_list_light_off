5f7c0770e7ed134a62eb3c75ddf114e8fd15d8eb
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-03-07 11:56:22 +0100

    feat: Implement app theme with light and dark modes

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list_light_off/theme/app_colors.dart';
+import 'package:shopping_list_light_off/theme/app_colors_extension.dart';
+import 'package:shopping_list_light_off/theme/app_text_styles.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      colorScheme: const ColorScheme.light(),
+      scaffoldBackgroundColor: AppColors.lightBackground,
+      dividerTheme: const DividerThemeData(
+        color: AppColors.lightBorderDefault,
+        space: 0,
+        thickness: 1,
+      ),
+      extensions: const [
+        AppColorsExtension(
+          background: AppColors.lightBackground,
+          surface: AppColors.lightSurface,
+          surfaceSecondary: AppColors.lightSurfaceSecondary,
+          scrim: AppColors.lightScrim,
+          textDefault: AppColors.lightTextDefault,
+          textWeak: AppColors.lightTextWeak,
+          iconDefault: AppColors.lightIconDefault,
+          iconHighlighted: AppColors.lightIconHighlighted,
+          borderDefault: AppColors.lightBorderDefault,
+          borderGradient: AppColors.lightBorderGradient,
+          lightBorderGradient: AppColors.lightBorderGradient,
+          darkBorderGradient: AppColors.darkBorderGradient,
+          skeleton: AppColors.lightSkeleton,
+        ),
+      ],
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      colorScheme: const ColorScheme.dark(),
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      dividerTheme: const DividerThemeData(
+        color: AppColors.darkBorderDefault,
+        space: 0,
+        thickness: 1,
+      ),
+      extensions: const [
+        AppColorsExtension(
+          background: AppColors.darkBackground,
+          surface: AppColors.darkSurface,
+          surfaceSecondary: AppColors.darkSurfaceSecondary,
+          scrim: AppColors.darkScrim,
+          textDefault: AppColors.darkTextDefault,
+          textWeak: AppColors.darkTextWeak,
+          iconDefault: AppColors.darkIconDefault,
+          iconHighlighted: AppColors.darkIconHighlighted,
+          borderDefault: AppColors.darkBorderDefault,
+          borderGradient: AppColors.darkBorderGradient,
+          lightBorderGradient: AppColors.lightBorderGradient,
+          darkBorderGradient: AppColors.darkBorderGradient,
+          skeleton: AppColors.darkSkeleton,
+        ),
+      ],
+    );
+  }
+}
+
+extension AppTheming on BuildContext {
+  AppColorsExtension get colors =>
+      Theme.of(this).extension<AppColorsExtension>()!;
+
+  AppTextStyles get textStyles => const AppTextStyles();
+}
