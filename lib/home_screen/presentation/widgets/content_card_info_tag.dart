6d033640b6ea82023f71ff16b12ff468ec5e4b82
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-03-08 03:59:30 +0100

    feat: Enhance ContentCardInfoTag with themed icon color

--- lib/home_screen/presentation/widgets/content_card_info_tag.dart
+++ lib/home_screen/presentation/widgets/content_card_info_tag.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_light_off/home_screen/presentation/widgets/scale_theme_element.dart';
+import 'package:shopping_list_light_off/theme/theme.dart';
 
 class ContentCardInfoTag extends StatelessWidget {
   const ContentCardInfoTag({super.key, required this.name, required this.icon});
@@ -9,11 +10,20 @@ class ContentCardInfoTag extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = context.colors;
+
     return ScaleThemeElement(
       child: Row(
         mainAxisSize: MainAxisSize.min,
         spacing: 6,
-        children: [Icon(icon, size: 16), Text(name)],
+        children: [
+          Icon(
+            icon,
+            size: 16,
+            color: colors.iconDefault.withValues(alpha: 0.7),
+          ),
+          Text(name),
+        ],
       ),
     );
   }
