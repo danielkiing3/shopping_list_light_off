7ef58c664ed94f343a8677eb28e4e5ec0ce8cb9d
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-03-12 12:50:58 +0100

    Adding new image asset and updating Card view model

--- lib/home_screen/model/card_view_model.dart
+++ lib/home_screen/model/card_view_model.dart
@@ -34,7 +34,18 @@ class CardDetailsSource {
       'assets/images/5.png',
       'assets/images/6.png',
     ],
-    allImages: [],
+    allImages: [
+      'assets/images/1.png',
+      'assets/images/2.png',
+      'assets/images/3.png',
+      'assets/images/4.png',
+      'assets/images/5.png',
+      'assets/images/6.png',
+      'assets/images/7.png',
+      'assets/images/2.png',
+      'assets/images/3.png',
+      'assets/images/4.png',
+    ],
   );
 
   static CardViewModel firstExample = CardViewModel(
@@ -49,6 +60,17 @@ class CardDetailsSource {
       'assets/images/2.png',
       'assets/images/3.png',
     ],
-    allImages: [],
+    allImages: [
+      'assets/images/1.png',
+      'assets/images/2.png',
+      'assets/images/3.png',
+      'assets/images/4.png',
+      'assets/images/5.png',
+      'assets/images/6.png',
+      'assets/images/7.png',
+      'assets/images/2.png',
+      'assets/images/3.png',
+      'assets/images/4.png',
+    ],
   );
 }
