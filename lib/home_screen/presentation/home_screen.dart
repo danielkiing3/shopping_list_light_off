988486b51712ea494eec9ead1ac90f5b53c5a41e
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-03-12 12:53:27 +0100

    Update home screen with dummy app chi[

--- lib/home_screen/presentation/home_screen.dart
+++ lib/home_screen/presentation/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_light_off/home_screen/model/card_view_model.dart';
+import 'package:shopping_list_light_off/shared/widgets/app_chip.dart';
 import 'widgets/widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,6 +17,15 @@ class HomeScreen extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             spacing: 14,
             children: [
+              Row(
+                spacing: 10,
+                children: [
+                  AppChip(data: 'All', isSelected: true),
+                  AppChip(data: 'Pinned'),
+                  AppChip(data: 'Recent'),
+                  AppChip(data: 'Shared'),
+                ],
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 4),
                 child: Center(child: Text('Pinned')),
